1d295c26e6f526b820dfdbd553697bf0b1aad0e5
BomNk <tanapon.bom@gmail.com>
2017-10-10 13:59:15 +0700

    000

--- Rubik.V1/Form1.cs
+++ Rubik.V1/Form1.cs
@@ -39,8 +39,8 @@ namespace Rubik.V1
         Image<Hsv, Byte> My_Image;
         String str = "";
         int numPicture = 1;
+        //TestGit
 
-        
         // รูปต้นฉบับ
         // Image<Hsv, Byte> gray_image;   // รูปที่โดนเปลี่ยนเป็นแบบ GRY
         Image<Hsv, Byte> My_image_copy; // รูปที่ใช้แก้ใข 
